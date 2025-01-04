b07a2a1985804fd2aaa34550964f6030913f9945
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2025-01-04 21:43:27 +0000

    update ReplenishHistory style

--- src/app/_components/authorizedPages/kazna/ReplenishHistory.tsx
+++ src/app/_components/authorizedPages/kazna/ReplenishHistory.tsx
@@ -2,23 +2,27 @@
 
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
-import {cn} from "~/lib/utils"
 
 import { api } from "~/trpc/react";
+
+import { cn } from "~/lib/utils";
 
 export default function ReplenishHistory() {
   const replenishHistory = api.kazna.getReplenishHistory.useQuery();
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="w-full">
       {replenishHistory.isFetching && (
         <Loader2 className="h-6 w-6 animate-spin text-[#b5b5b5]" />
       )}
 
       {!replenishHistory.isFetching &&
-        replenishHistory.data?.map((replenish) => (
+        replenishHistory.data?.map((replenish, i) => (
           <div
-            className="flex justify-between items-center bg-accent py-2 px-4 rounded-md gap-44 max-w-[500px]"
+            className={cn(
+              "flex justify-between items-center bg-accent py-2 px-4 rounded-md ml-auto max-w-[500px]",
+              i !== 0 && "mt-6",
+            )}
             key={replenish.id}
           >
             <div className="flex flex-col gap-2">
@@ -28,7 +32,7 @@ export default function ReplenishHistory() {
                   width={32}
                   height={32}
                   alt="avatar"
-                  className="rounded-full w-8"
+                  className="rounded-full w-8 h-8"
                 />
 
                 <h1>
@@ -45,7 +49,14 @@ export default function ReplenishHistory() {
               </div>
             </div>
 
-            <h1 className={cn("text-xl", replenish.amount > 0 ? "text-green-400" : "text-red-400")}>{replenish.amount}</h1>
+            <h1
+              className={cn(
+                "text-xl",
+                replenish.amount > 0 ? "text-green-400" : "text-red-400",
+              )}
+            >
+              {replenish.amount}
+            </h1>
           </div>
         ))}
     </div>
