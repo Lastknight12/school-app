46ee9299d49f0c82bd441b2ad3b5281aa4576490
Lastknight12 <vovaua27@gmail.com>
2025-02-20 10:24:44 +0200

    add access check in teachers table

--- src/app/(authorized)/klass/[id]/page.tsx
+++ src/app/(authorized)/klass/[id]/page.tsx
@@ -1,9 +1,20 @@
+import { notFound } from "next/navigation";
+
+import { getServerAuthSession } from "~/server/auth";
+
 import KlassTable from "~/app/_components/authorizedPages/klass/[id]/KlassTable";
 
-type Params = {id: string}
+type Params = { id: string };
 
 export default async function Page({ params }: { params: Params }) {
-  const {id} = params
+  const { id } = params;
+  const session = await getServerAuthSession();
+  if (
+    session?.user.role === "TEACHER" &&
+    !session.user.teacherClasses.some((klass) => klass.id === id)
+  ) {
+    return notFound();
+  }
 
   return <KlassTable id={id} />;
 }
