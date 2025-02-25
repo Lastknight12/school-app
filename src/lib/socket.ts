4d41449250494be6e68911836c47da2c4f21f3e4
Lastknight12 <vovaua27@gmail.com>
2025-02-25 09:39:42 +0200

    update env / dev dep

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,9 @@
+"use client";
+
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001/", {
+import { env } from "../env";
+
+export const socket = io(env.SOCKET_URL, {
   withCredentials: true,
 });
