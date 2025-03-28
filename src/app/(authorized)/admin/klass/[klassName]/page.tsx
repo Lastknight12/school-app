852f18cdf45229143c394a3e1f65f1eee898d15d
Lastknight12 <vovaua27@gmail.com>
2025-03-28 18:00:03 +0200

    update adminData api && restore /admin/klass/* page

--- src/app/(authorized)/admin/klass/[klassName]/page.tsx
+++ src/app/(authorized)/admin/klass/[klassName]/page.tsx
@@ -1,3 +1,40 @@
-export default function Page() {
-  return "asd";
+import { notFound } from "next/navigation";
+
+import getAdminData from "~/server/callers/klass/adminData/server";
+
+import ButtonsGroup from "./_components/ButtonsGroup";
+import StudentsTable from "./_components/StudentTable";
+import { StudentTableSearch } from "./_components/StudentTableSearch";
+
+export default async function Page({
+  params,
+}: {
+  params: { klassName: string };
+}) {
+  const decodedName = decodeURIComponent(params.klassName);
+
+  const klass = await getAdminData({ name: decodedName });
+
+  if (!klass) {
+    return notFound();
+  }
+
+  return (
+    <main className="px-6">
+      <StudentsTable klassId={klass.id}>
+        {/* Table header */}
+
+        <div className="flex items-ceter justify-between">
+          <StudentTableSearch />
+
+          <ButtonsGroup
+            klassId={klass.id}
+            klassName={decodedName}
+            initStudents={klass.students}
+            initTeachers={klass.teachers}
+          />
+        </div>
+      </StudentsTable>
+    </main>
+  );
 }
