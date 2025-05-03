bcacf4ee1605b5a4ebe8473b657f7fd19fb5b8b3
Lastknight12 <vovaua27@gmail.com>
2025-05-03 14:24:07 +0300

    Update environment variable for socket server URL to use NEXT_PUBLIC prefix

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,6 +4,6 @@ import { io } from "socket.io-client";
 
 import { env } from "../env";
 
-export const socket = io(env.SOCKET_URL, {
+export const socket = io(env.NEXT_PUBLIC_SOCKET_URL, {
   withCredentials: true,
 });
