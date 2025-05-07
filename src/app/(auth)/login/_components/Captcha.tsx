98ac0f6caed0504f4d3c500306e80d10e7b569b1
Lastknight12 <vovaua27@gmail.com>
2025-05-07 13:31:59 +0300

    add recaptcha in login form

---
+++ src/app/(auth)/login/_components/Captcha.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { toast } from "sonner";
+import { env } from "~/env";
+
+interface Props {
+  onSuccess: () => void;
+}
+
+export default function Captcha({ onSuccess }: Props) {
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
+
+  async function handleCaptchaSubmission(token: string | null) {
+    try {
+      if (token) {
+        const response = await fetch("/api/auth/captcha", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ token }),
+        });
+
+        const responseData = (await response.json()) as {
+          message: string;
+        };
+
+        if (responseData.message === "Success") onSuccess();
+      }
+    } catch (e) {
+      toast.error("Помилка перевірки капчі. Спробуйте ще раз.");
+    }
+  }
+
+  const handleChange = (token: string | null) => {
+    void handleCaptchaSubmission(token);
+  };
+
+  function handleExpired() {
+    toast.error("Термін дії капчі закінчився. Спробуйте ще раз.");
+    recaptchaRef.current?.reset();
+  }
+
+  return (
+    <ReCAPTCHA
+      sitekey={env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+      ref={recaptchaRef}
+      onChange={handleChange}
+      onExpired={handleExpired}
+    />
+  );
+}
